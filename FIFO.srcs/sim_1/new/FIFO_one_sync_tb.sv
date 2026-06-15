8a67b512d994f1597aa409967ffc5a02f799cb90
JCOD100 <ewanhrykin@yandex.ru>
2026-06-15 22:49:23 +0300

    adding a FIFO with a single sync signal and a testbench for it

---
+++ FIFO.srcs/sim_1/new/FIFO_one_sync_tb.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 15.06.2026 21:49:44
+// Design Name: 
+// Module Name: FIFO_one_sync_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module FIFO_one_sync_tb(
+
+    );
+endmodule
---
+++ FIFO.srcs/sources_1/new/FIFO_one_sync.v
@@ -0,0 +1,6 @@
+`timescale 1ns / 1ps
+
+module FIFO_one_sync(
+
+    );
+endmodule
